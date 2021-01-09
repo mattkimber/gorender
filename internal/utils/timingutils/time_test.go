8d8678bd2d1b6285e23b05517a15207a41f32903
Matt Kimber <github@mattkimber.org.uk>
2021-01-09 17:37:08 +0000

    make mac test more lenient

--- internal/utils/timingutils/time_test.go
+++ internal/utils/timingutils/time_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestTime(t *testing.T) {
 	ms := Time("test", false, func() { time.Sleep(50 * time.Millisecond) })
-	if ms < 50 || ms > 100 {
-		t.Errorf("Expected value in range [50,100], got %d", ms)
+	if ms < 50 || ms > 250 {
+		t.Errorf("Expected value in range [50,250], got %d", ms)
 	}
 }
