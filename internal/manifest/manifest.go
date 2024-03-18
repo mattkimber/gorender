394a67a8f595be46fe0e8e022a64f2c855c8cc7b
Yi Yang @ Anteros <ahyangyi@gmail.com>
2024-03-18 13:40:53 +0800

    Implement shadow_threshold and soft_shadow options

--- internal/manifest/manifest.go
+++ internal/manifest/manifest.go
@@ -63,6 +63,8 @@ type Manifest struct {
 	MaxPush                   int              `json:"max_colour_push"`
 	Fosterise                 bool             `json:"fosterise"`
 	NoEdgeFosterisation       bool             `json:"suppress_edge_fosterisation"`
+	SoftShadow                bool             `json:"soft_shadow"`
+	ShadowThreshold           float64          `json:"shadow_threshold"`
 }
 
 func FromJson(handle io.Reader) (manifest Manifest, err error) {
