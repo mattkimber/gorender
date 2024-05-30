d7a53a492c39ab82cd6e2737da0d13a5437218bf
mattkimber <github@mattkimber.org.uk>
2024-05-30 19:48:48 +0100

    fix tests failing on mac in Github actions

--- internal/sampler/sampler_test.go
+++ internal/sampler/sampler_test.go
@@ -2,7 +2,7 @@ package sampler
 
 import (
 	"github.com/mattkimber/gorender/internal/geometry"
-	"reflect"
+	"math"
 	"testing"
 )
 
@@ -22,8 +22,37 @@ func TestSquare(t *testing.T) {
 		}},
 	}
 
-	if gotResult := Square(2, 1, 2, 0, 0.5); !reflect.DeepEqual(gotResult, expected) {
-		t.Errorf("Square() = %v, want %v", gotResult, expected)
+	// This test is infuriatingly clunky due to Mac and Win/Linux returning different floating point
+	// roundings in GitHub actions
+	res := Square(2, 1, 2, 0, 0.5)
+	if len(res) != len(expected) {
+		t.Errorf("Square() arrays not even, got %d expected %d", len(res), len(expected))
+	}
+
+	for i, _ := range res {
+		if len(res[i]) != len(expected[i]) {
+			t.Errorf("Square() %d arrays not even, got %d expected %d", i, len(res[i]), len(expected[i]))
+		}
+
+		for j, _ := range res[i] {
+			if len(res[j]) != len(expected[j]) {
+				t.Errorf("Square() %d.%d arrays not even, got %d expected %d", i, j, len(res[j]), len(expected[j]))
+			}
+
+			for k, _ := range res[j] {
+				if math.Abs(expected[i][j][k].Location.X-res[i][j][k].Location.X) > 0.00001 {
+					t.Errorf("Square() %d.%d.%d X = %f, want %f", i, j, k, res[i][j][k].Location.X, expected[i][j][k].Location.X)
+				}
+
+				if math.Abs(expected[i][j][k].Location.Y-res[i][j][k].Location.Y) > 0.00001 {
+					t.Errorf("Square() %d.%d.%d Y = %f, want %f", i, j, k, res[i][j][k].Location.Y, expected[i][j][k].Location.Y)
+				}
+
+				if math.Abs(expected[i][j][k].Influence-res[i][j][k].Influence) > 0.00001 {
+					t.Errorf("Square() %d.%d.%d Z = %f, want %f", i, j, k, res[i][j][k].Influence, expected[i][j][k].Influence)
+				}
+			}
+		}
 	}
 }
 
